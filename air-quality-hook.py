69a95a7b292d79d6a9cd063d34a805f0e4354724
Ian Bolliger <bolliger@berkeley.edu>
2024-02-01 15:33:25 -0800

    fix environ brackets

--- air-quality-hook.py
+++ air-quality-hook.py
@@ -3,14 +3,14 @@ import os
 import pandas as pd
 import requests
 
-BEARER = os.environ("AWAIR_BEARER")
-DEVICE_ID = os.environ("AWAIR_DEVICE_ID")
-PM_HI_EVENT_NAME = os.environ("IFTTT_AWAIR_PM_HI_EVENT")
-PM_LOW_EVENT_NAME = os.environ("IFTTT_AWAIR_PM_LOP)EVENT")
-WEBHOOKS_KEY = os.environ("IFTTT_WEBHOOKS_KEY")
+BEARER = os.environ["AWAIR_BEARER"]
+DEVICE_ID = os.environ["AWAIR_DEVICE_ID"]
+PM_HI_EVENT_NAME = os.environ["IFTTT_AWAIR_PM_HI_EVENT"]
+PM_LOW_EVENT_NAME = os.environ["IFTTT_AWAIR_PM_LOP)EVENT"]
+WEBHOOKS_KEY = os.environ["IFTTT_WEBHOOKS_KEY"]
 
 
-def query_endpoint():
+def query_endpoint() -> pd.Series:
     return pd.DataFrame(
         requests.get(
             "https://developer-apis.awair.is/v1/users/self/devices/awair-element/"
@@ -20,13 +20,13 @@ def query_endpoint():
     ).set_index("comp")["value"]
 
 
-def check_pm(data):
+def check_pm(data: pd.Series) -> bool:
     if data.pm25 > 0:
         return True
     return False
 
 
-def send_webhook(high=True):
+def send_webhook(high: bool = True) -> requests.Request:
     if high:
         event_name = PM_HI_EVENT_NAME
     else:
